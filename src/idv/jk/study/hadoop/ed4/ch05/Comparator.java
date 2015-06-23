da12134f00047186e7a2204ace10399081f0ede9
javakidx <javakidx@gmail.com>
2015-06-23 09:11:24 +0800

    first commit

--- src/idv/jk/study/hadoop/ed4/ch05/Comparator.java
+++ src/idv/jk/study/hadoop/ed4/ch05/Comparator.java
@@ -11,35 +11,35 @@ import java.io.IOException;
  */
 public class Comparator extends WritableComparator
 {
-    private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
-
-    public Comparator()
-    {
-        super(TextPair.class);
-    }
-
-    @Override
-    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2)
-    {
-        try
-        {
-            int firstL1 = WritableUtils.decodeVIntSize(b1[s1])  + readVInt(b1, s1);
-            int firstL2 = WritableUtils.decodeVIntSize(b2[s2])  + readVInt(b2, s2);
-
-            int cmp = TEXT_COMPARATOR.compare(b1, s1, firstL1, b2, s2, firstL2);
-            if(cmp != 0)
-            {
-                return cmp;
-            }
-
-            return TEXT_COMPARATOR.compare(b1, s1 + firstL1, l1 - firstL1,
-                                            b2, s2 + firstL2, l2 - firstL2);
-        } catch (IOException e)
-        {
-            throw new IllegalArgumentException(e);
-        }
-
-    }
+//    private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
+//
+//    public Comparator()
+//    {
+//        super(TextPair.class);
+//    }
+//
+//    @Override
+//    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2)
+//    {
+//        try
+//        {
+//            int firstL1 = WritableUtils.decodeVIntSize(b1[s1])  + readVInt(b1, s1);
+//            int firstL2 = WritableUtils.decodeVIntSize(b2[s2])  + readVInt(b2, s2);
+//
+//            int cmp = TEXT_COMPARATOR.compare(b1, s1, firstL1, b2, s2, firstL2);
+//            if(cmp != 0)
+//            {
+//                return cmp;
+//            }
+//
+//            return TEXT_COMPARATOR.compare(b1, s1 + firstL1, l1 - firstL1,
+//                                            b2, s2 + firstL2, l2 - firstL2);
+//        } catch (IOException e)
+//        {
+//            throw new IllegalArgumentException(e);
+//        }
+//
+//    }
 }
 
 //static

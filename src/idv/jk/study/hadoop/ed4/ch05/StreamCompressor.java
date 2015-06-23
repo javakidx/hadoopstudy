e0efed4c41a2973d514681204869931b785e22a7
javakidx <javakidx@gmail.com>
2015-06-23 21:27:42 +0800

    Chapter03

---
+++ src/idv/jk/study/hadoop/ed4/ch05/StreamCompressor.java
@@ -0,0 +1,28 @@
+package idv.jk.study.hadoop.ed4.ch05;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import java.io.IOException;
+
+/**
+ * Created by javakid on 2015/6/19.
+ */
+public class StreamCompressor
+{
+    public static void main(String[] args) throws ClassNotFoundException, IOException
+    {
+        String codecClassName = args[0];
+        Class<?> codecClass = Class.forName(codecClassName);
+
+        Configuration conf = new Configuration();
+        CompressionCodec codec = (CompressionCodec) ReflectionUtils.newInstance(codecClass, conf);
+
+        CompressionOutputStream out = codec.createOutputStream(System.out);
+        IOUtils.copyBytes(System.in, out, 4096, false);
+        out.close();
+    }
+}
